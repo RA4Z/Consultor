31eef6f7b766cc77edb3980669536539e8f7edde
RA4Z <rraz639@gmail.com>
2021-10-01 08:54:55 -0300

    Mudança nos Inputs

--- src/main/java/br/com/weg/api/model/input/ApontamentoInputDTO.java
+++ src/main/java/br/com/weg/api/model/input/ApontamentoInputDTO.java
@@ -3,11 +3,17 @@ package br.com.weg.api.model.input;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
 @Getter
 @Setter
 public class ApontamentoInputDTO {
 
+    @NotBlank
     private String dataRegistro;
+    @NotNull
     private int horas;
+    @NotBlank
     private String descricao;
 }
