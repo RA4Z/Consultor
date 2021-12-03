406c106444ba06b26480186e71e0564c87696b4c
RA4Z <82897913+RA4Z@users.noreply.github.com>
2021-12-03 08:00:02 -0300

    Atualização na migration dos cards

--- src/main/resources/db/migration/V1631197937426__createTableCards.sql
+++ src/main/resources/db/migration/V1631197937426__createTableCards.sql
@@ -10,12 +10,11 @@ CREATE TABLE cards
     card_tempolimite  INT NOT NULL,
     card_horas        double NOT NULL,
     horas_enviado     INT,
-    horas_aprovado    INT,
     PRIMARY KEY (id)
 );
-    INSERT INTO cards VALUES (1,1,"Seção XBY","Projeção de Sistemas","Parado","1111-11-11","1000-10-12",200,0,0,0);
-    INSERT INTO cards VALUES (2,1,"Seção ABC","Projeção de Sistemas","Em Andamento","1111-11-11","1000-10-12",150,0,0,0);
-    INSERT INTO cards VALUES (3,1,"Seção TRA","Automação de Motores","Completo","1111-11-11","1000-10-12",100,0,0,0);
-    INSERT INTO cards VALUES (4,1,"Seção RAZ","Desenvolvimento de Software","Não Iniciado","1111-11-11","1000-10-12",300,0,0,0);
+    INSERT INTO cards VALUES (1,1,"Seção XBY","Projeção de Sistemas","Parado","1111-11-11","1000-10-12",200,0,0);
+    INSERT INTO cards VALUES (2,1,"Seção ABC","Projeção de Sistemas","Em Andamento","1111-11-11","1000-10-12",150,0,0);
+    INSERT INTO cards VALUES (3,1,"Seção TRA","Automação de Motores","Completo","1111-11-11","1000-10-12",100,0,0);
+    INSERT INTO cards VALUES (4,1,"Seção RAZ","Desenvolvimento de Software","Não Iniciado","1111-11-11","1000-10-12",300,0,0);
 ALTER TABLE cards
     ADD CONSTRAINT fk_cards_usuario FOREIGN KEY (usuario_id) REFERENCES usuario (id);
